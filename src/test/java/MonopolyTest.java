f5bca1adba6fc1c294150e7c1849bfaf12eb2394
02Ian <mohitathani15598@gmail.com>
2021-12-24 13:39:58 +0530

    Junit Completed for Spy Random Number

--- src/test/java/MonopolyTest.java
+++ src/test/java/MonopolyTest.java
@@ -4,8 +4,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class MonopolyTest {
     Monopoly player = new Monopoly(0);
@@ -13,10 +12,17 @@ class MonopolyTest {
     @Test
     public void when_die_rolls_are_1_and_2_current_position_should_advance_to_3(){
         //WRITE CODE HERE
+        Monopoly spiedPlayer = Mockito.spy(player);
+        when(spiedPlayer.throwDie()).thenReturn(2,1);
+        int response = spiedPlayer.advance();
+        assertEquals(3,response);
     }
     @Test
     public void when_die_rolls_are_3_and_3_advance_should_be_called_again_and_when_1_and_3_are_rolled_current_position_should_be_updated_to_10(){
         // WRITE CODE HERE
+        Monopoly spiledDies = Mockito.spy(player);
+        when(spiledDies.throwDie()).thenReturn(3,3, 3, 1);
+        int response = spiledDies.advance();
+        assertEquals(10,response);
     }
-
 }
